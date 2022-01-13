6fbc434a54aadd74d454b04204a700f57589fc4f
Yigit S <55548182+yigitsr@users.noreply.github.com>
2022-01-13 14:57:59 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,37 +1,45 @@
-const btnPop = document.getElementById('borderPop')
-const btnSlide = document.getElementById('backSlide')
-const btnCircle = document.getElementById('backCircle')
-const btnUnder = document.getElementById('borderUnder')
+		const btnPop = document.getElementById('borderPop')
+        const btnSlide = document.getElementById('backSlide')
+        const btnCircle = document.getElementById('backCircle')
+        const btnUnder = document.getElementById('borderUnder')
+        const btnSand = document.getElementById('slidingSand')
 
-btnPop.addEventListener('click',() => {
-  alert('test')
+		btnPop.addEventListener('click',() => {
 
-  navigator.clipboard.writeText(borderPop.innerText)
-})
+		  navigator.clipboard.writeText(borderCSS.innerText)
 
-btnSlide.addEventListener('click',() => {
-  alert('test')
+		})
 
-  navigator.clipboard.writeText(borderPop.innerText)
-})
+        btnSlide.addEventListener('click',() => {
 
-btnCircle.addEventListener('click',() => {
-  alert('test')
+		  navigator.clipboard.writeText(backSlideCSS.innerText)
+		})
 
-  navigator.clipboard.writeText(borderPop.innerText)
-})
+        btnCircle.addEventListener('click',() => {
 
-btnUnder.addEventListener('click',() => {
-  alert('test')
+		  navigator.clipboard.writeText(backCircleCSS.innerText)
+		})
+        
+        btnUnder.addEventListener('click',() => {
 
-  navigator.clipboard.writeText(borderPop.innerText)
-})
+		  navigator.clipboard.writeText(borderUnderCSS.innerText)
+		})
 
-// when click a button, copy the text to clipboard
-const btn = document.querySelectorAll('.btn')
-btn.forEach(btn => {
-    btn.addEventListener('click', () => {
-        navigator.clipboard.writeText(backSlide.innerText)
-    })
-})
 
+
+        btnSand.addEventListener('click',() => {   
+
+            navigator.clipboard.writeText(slidingCSS.innerText)
+        })
+
+        
+        // when click a button, copy the text to clipboard
+        // const btn = document.querySelectorAll('.btn')
+        // btn.forEach(btn => {
+        //     btn.addEventListener('click', () => {
+        //         navigator.clipboard.writeText(borderPop.innerText)
+        //         alert('test')
+        //     })
+        // })
+
+    	
