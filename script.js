f1e824124e33d43639bedaf0e2c3bcc9027d784a
Yigit S <55548182+yigitsr@users.noreply.github.com>
2022-01-13 14:58:43 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-		const btnPop = document.getElementById('borderPop')
+	const btnPop = document.getElementById('borderPop')
         const btnSlide = document.getElementById('backSlide')
         const btnCircle = document.getElementById('backCircle')
         const btnUnder = document.getElementById('borderUnder')
@@ -30,16 +30,4 @@
         btnSand.addEventListener('click',() => {   
 
             navigator.clipboard.writeText(slidingCSS.innerText)
-        })
-
-        
-        // when click a button, copy the text to clipboard
-        // const btn = document.querySelectorAll('.btn')
-        // btn.forEach(btn => {
-        //     btn.addEventListener('click', () => {
-        //         navigator.clipboard.writeText(borderPop.innerText)
-        //         alert('test')
-        //     })
-        // })
-
-    	
+        }) 	
